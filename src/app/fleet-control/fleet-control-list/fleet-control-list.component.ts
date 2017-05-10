421dd175ae3b5c35cc0c15b97ef60fa18f478c85
Jean Franco Pizarro Gonçalves <jegoncalves@fh.com.br>
2017-05-10 15:44:01 -0300

    Adjust pagination logic

--- src/app/fleet-control/fleet-control-list/fleet-control-list.component.ts
+++ src/app/fleet-control/fleet-control-list/fleet-control-list.component.ts
@@ -19,13 +19,32 @@ export class FleetControlListComponent implements OnInit {
   hasCheckboxChecked: boolean = false;
 
   // pagination
+
+	/**
+	 * Page in exibition in list
+	 */
   pageNow: number = 1;
+
+	/**
+	 * Array with pages for pagination
+	 */
   pagination: any[] = [];
 
   /**
-   * elements per page 
+   * Elements per page 
    **/
   perPage: number = 5
+
+	/**
+	 * Elements in pagination
+	 */
+	pagesInPagination: number = 5;
+
+	/**
+	 * Total Pages
+	 * Ps.: Generate on init
+	 */
+	totalPages: number = 0;
 
   constructor(
     private fleetControlService: FleetControlService,
@@ -36,15 +55,14 @@ export class FleetControlListComponent implements OnInit {
     
     this.fleetControlService.findAll()
         .then((veiculos: Veiculo[]) => {
-            this.createPagination(veiculos);
             this._veiculos = veiculos;
-
             this.veiculos = veiculos.slice(0, this.perPage);
+						this.totalPages = Math.ceil(veiculos.length / this.perPage);
+            this.createPagination();
         })
         .catch(err => {
             console.error(err);
         });
-      
   }
 
   onChangeCbxAll(): void {
@@ -81,10 +99,10 @@ export class FleetControlListComponent implements OnInit {
   onSearch(term: any): void {
       this.fleetControlService.search(term.value)
           .then((veiculos: Veiculo[]) => {
-              this.createPagination(veiculos);
               this._veiculos = veiculos;
               this.veiculos = veiculos.slice(0, this.perPage);
               this.pageNow = 1;
+              this.createPagination();
               this.onChangeCbxAll();
           })
           .catch(err => {
@@ -92,18 +110,32 @@ export class FleetControlListComponent implements OnInit {
           });
   }
 
-  private createPagination(veiculos: Veiculo[]): void {
-    let count = veiculos.length;
+  private createPagination(): void {
     this.pagination = [];
-    for(let i = 1; i <= Math.ceil(count / this.perPage); i++) {
-      this.pagination.push(i);
+    for(let i = 1; i <= this.totalPages; i++) {
+			this.pagination.push(i);
     }
+
+		if (this.pagination.length > this.pagesInPagination) {
+			let index = this.pagination.indexOf(this.pageNow) - Math.floor(this.pagesInPagination/2);
+			if (index < 0) {
+				index = 0;
+			}
+			
+			let lastIndex = this.pagination.length - this.pagesInPagination;
+			if (index > lastIndex) {
+				index = lastIndex;
+			}
+
+			this.pagination = this.pagination.splice(index, this.pagesInPagination);
+		}
   }
 
   onChangePage(page: number): void {
     this.veiculos = this._veiculos.slice((page - 1) * this.perPage, this.perPage * page);
     this.pageNow = page;
     this.checkboxHead = false;
+		this.createPagination();
     this.onChangeCbxAll();
   }
 
